d5aea3fff3633f8bc0f4ba6b6a1b6dcee0a974bc
vjrj <vjrj@ourproject.org>
2015-10-12 03:08:32 +0200

    New icons for new persons

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -29,10 +29,12 @@ Template.home.onCreated(function() {
       var lat = person.lugarNacimientoLatitud;
       var long = person.lugarNacimientoLongitud;
       if (isValidLatLng(lat) && isValidLatLng(long)) {
+        //console.log("Adding new marker to map");
+        var isNew = person.updatedAt > lastDay();
         var marker = new google.maps.Marker({
           map: map.instance,
           // https://developers.google.com/maps/documentation/javascript/examples/map-latlng-literal
-          icon: person.buscasBebe? '/images/gmaps-pointer.png': '/images/gmaps-pointer-familia.png',
+          icon: isNew ? '/images/gmaps-pointer-new.png': person.buscasBebe? '/images/gmaps-pointer.png': '/images/gmaps-pointer-familia.png',
           position:  {lat: parseInt(lat), lng: parseInt(long)}
         });
 
@@ -42,31 +44,26 @@ Template.home.onCreated(function() {
         google.maps.event.addListener(marker, 'click', (function(marker, person) {
           return function() {
             infowindow.setContent("<a href='/bebe/" + person._id + "/'>" +
-                                    (person.buscasBebe?
-                                     "Buscamos bebe ":
-                                     "Busco a mi familia biológica ") +
-                                  renderSexoAlt(person.sexo) +
-                                  (person.fechaNacimiento instanceof Date? (person.buscasBebe? " nacido aquí el ": " nací aquí el "): " nacido aquí ") +
-                                  renderAprox(person.fechaNacimientoEsAprox) +
-                                  renderDate(person.fechaNacimiento) +
-                                  "</a>");
+                                    (person.buscasBebe? "Buscamos bebe ":
+                                     "Busco a mi familia biológica ") + renderSexoAlt(person.sexo) +
+                                      (person.fechaNacimiento instanceof Date?
+                                       (person.buscasBebe? " nacido aquí el ": " nací aquí el "): " nacido aquí ") +
+                                  renderAprox(person.fechaNacimientoEsAprox) + renderDate(person.fechaNacimiento) +
+                 (isNew? " <span class='label label-warning'>Nuevo</span>" : "") + "</a>");
             infowindow.open(map.instance, marker);
           }
         })(marker, person));
       }
     };
 
-    var cursor = Persons.find();
-    cursor.forEach(function (person) {
-      createMarker(map, person);
-    });
-
     Persons.find().observe({
       // http://meteorcapture.com/how-to-create-a-reactive-google-map/
       added: function(document) {
+        //console.log("Added new marker");
         createMarker(map, document);
       },
       changed: function(newDocument, oldDocument) {
+        // console.log("Changed marker");
         markers[newDocument._id].setPosition({ lat: parseInt(newDocument.lugarNacimientoLatitud),
                                                lng: parseInt(newDocument.lugarNacimientoLongitud) });
       },
