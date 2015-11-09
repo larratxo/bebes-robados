4c012376fc5a7dfa83511199984506d05e0b744e
vjrj <vjrj@ourproject.org>
2015-11-09 05:37:40 +0100

    Allow blob for image drop

--- server/config/security.js
+++ server/config/security.js
@@ -3,8 +3,13 @@ BrowserPolicy.content.allowOriginForAll("*.gstatic.com");
 BrowserPolicy.content.allowOriginForAll("*.bootstrapcdn.com");
 BrowserPolicy.content.allowOriginForAll("*.gravatar.com");
 
+BrowserPolicy.content.allowOriginForAll("*.comunes.org");
+
 BrowserPolicy.content.allowFontDataUrl();
 
 // Needed by yogiben:autoform-map but a security risk
 // https://github.com/yogiben/meteor-autoform-modals/issues/20
 BrowserPolicy.content.allowEval();
+
+// https://stackoverflow.com/questions/32600469/meteor-browser-policy-local-camera-not-allowed
+BrowserPolicy.content.allowOriginForAll('blob:');
