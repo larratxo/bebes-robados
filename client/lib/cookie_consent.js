dfd80141f773c58c9276cf8a9897d2272aada981
vjrj <vjrj@ourproject.org>
2015-11-09 01:15:01 +0100

    EU Cookies message

---
+++ client/lib/cookie_consent.js
@@ -0,0 +1,13 @@
+var options = {
+  cookieTitle: "Usamos Cookies",
+  cookieMessage: "Usamos cookies para asegurar un mejor uso de nuestra web.",
+  cookieMessageImply: "Al continuar usando nuestra web sin cambiar la configuración, estas aceptando que usemos estas cookies.",
+  showLink: false,
+  position: 'bottom',
+  linkText: "Lee más",
+  linkRouteName: "/",
+  html: false,
+  expirationInDays: 70
+};
+
+CookieConsent._config = options;
