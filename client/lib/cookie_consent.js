0b789dcb2e4f2793977b33a6661bfd72c21cd3ad
vjrj <vjrj@ourproject.org>
2015-11-09 02:33:00 +0100

    EU Cookies

--- client/lib/cookie_consent.js
+++ client/lib/cookie_consent.js
@@ -1,13 +1,15 @@
 var options = {
-  cookieTitle: "Usamos Cookies",
+  cookieTitle: "Uso de Cookies",
   cookieMessage: "Usamos cookies para asegurar un mejor uso de nuestra web.",
-  cookieMessageImply: "Al continuar usando nuestra web sin cambiar la configuración, estas aceptando que usemos estas cookies.",
+  // cookieMessageImply: "Al continuar usando nuestra web sin cambiar la configuración, estas aceptando que usemos estas cookies.",
   showLink: false,
-  position: 'bottom',
+  position: 'top',
   linkText: "Lee más",
   linkRouteName: "/",
+  acceptButtonText: "Aceptar",
   html: false,
   expirationInDays: 70
 };
 
+// CookieConsent.init();
 CookieConsent._config = options;
