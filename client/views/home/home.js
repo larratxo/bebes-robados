1962b55bae6025f7032aae9e5a51e4b6843bd76f
vjrj <vjrj@ourproject.org>
2015-12-19 13:35:59 +0100

    Home map grey styled

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -9,6 +9,8 @@ Template.home.helpers({
         zoomControl: true,
         // Sin nombres ni carreteras
         // styles: [{ featureType: "road", stylers: [  {visibility: "on"} ] } ],
+        styles: [{"featureType":"all","elementType":"all","stylers":[{"saturation":-100},{"gamma":0.5}]}],
+        /* styles: [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}], */
         zoom: 5
         // Maybe:
         // https://stackoverflow.com/questions/9555499/apply-mask-to-google-map
