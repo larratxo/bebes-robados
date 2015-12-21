54a515c632fd458f9aca875162710194d915e9b0
Vicente J. Ruiz Jurado <vjrj@ourproject.org>
2015-12-21 13:23:47 +0100

    Adding google and twitter account (wip)

--- server/config/security.js
+++ server/config/security.js
@@ -1,7 +1,9 @@
 BrowserPolicy.content.allowOriginForAll("*.googleapis.com");
+BrowserPolicy.content.allowOriginForAll("*.googleusercontent.com");
 BrowserPolicy.content.allowOriginForAll("*.gstatic.com");
 BrowserPolicy.content.allowOriginForAll("*.bootstrapcdn.com");
 BrowserPolicy.content.allowOriginForAll("*.gravatar.com");
+BrowserPolicy.content.allowOriginForAll("*.twimg.com");
 
 BrowserPolicy.content.allowOriginForAll("*.comunes.org");
 
