4d44914609d82053bca0220ae069bd4c9646525a
vjrj <vjrj@ourproject.org>
2015-12-28 12:15:23 +0100

    Better roles usage

--- client/views/header.js
+++ client/views/header.js
@@ -1,7 +1,2 @@
 Template.header.onRendered(function () {
 });
-
-Template.header.helpers({
-    // check if user is an admin
-    isAdminUser: isAdminUser()
-})
--- client/views/roles/admins-ui.js
+++ client/views/roles/admins-ui.js
@@ -1,8 +1,2 @@
-isAdminUser = function() {
-        return Roles.userIsInRole(Meteor.user(), ['admin']);
-}
-
 Template.adminTemplate.helpers({
-    // check if user is an admin
-    isAdminUser: isAdminUser()
 })
