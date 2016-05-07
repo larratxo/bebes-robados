362145884b6eb6a6cd78cff9fb03e63f4bf2d97e
vjrj <vjrj@ourproject.org>
2016-05-07 09:40:51 +0200

    Fixing some tests

--- client/startup/default.js
+++ client/startup/default.js
@@ -17,7 +17,7 @@ Meteor.startup(function () {
       type: 'success',
       style: 'growl-top-right',
       title: 'Estamos en pruebas',
-      message: 'Puedes meter datos de bebes para testear pero ten en cuenta que durante la fase de pruebas los borraremos de tanto en tanto');
+      message: 'Puedes meter datos de bebes para testear pero ten en cuenta que durante la fase de pruebas los borraremos de tanto en tanto'});
     Bert.defaults = {
       hideDelay: 3500
     };
