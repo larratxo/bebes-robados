c07bc7b142d20d016b7a18ef0ac0037e41513256
vjrj <vjrj@ourproject.org>
2016-05-08 09:03:59 +0200

    More work with notifs

---
+++ server/config/emailNotif.js
@@ -0,0 +1,36 @@
+/* global Email Roles Meteor check */
+
+Meteor.methods({
+  sendNotifToRole: function (role, subject, body) {
+    check(role, String);
+    check(subject, String);
+    check(body, String);
+    check(this.userId, String);
+
+    console.log('Notificacion generada por usuario: ' + this.userId);
+
+    // console.log(comment);
+    var admins = Roles.getUsersInRole(role);
+    var verifiedEmails = [];
+    for (var i = 0; i < admins.length; i++) {
+      var emails = admins[i].emails;
+      for (var j = 0; j < emails.length; j++) {
+        if (emails[j].verified) {
+          verifiedEmails.push(emails[j].address);
+        }
+      }
+    }
+
+    for (var k = 0; k < verifiedEmails.length; k++) {
+      var email = verifiedEmails[k];
+      Email.send({
+        from: 'Bebes Robados <noreply@comunes.org>',
+        to: email.address,
+        subject: subject,
+        text: body + '\n\n(...)\n\Accede en: ' + process.env.ROOT_URL
+      });
+    }
+
+    return;
+  }
+});
