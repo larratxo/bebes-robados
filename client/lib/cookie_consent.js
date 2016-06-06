7c7a0985710e1f2b5fb798c59f94afb67512deef
vjrj <vjrj@ourproject.org>
2016-06-06 13:19:27 +0200

    Updated cookie-consent

--- client/lib/cookie_consent.js
+++ client/lib/cookie_consent.js
@@ -1,15 +1,16 @@
+/* global CookieConsent */
+
 var options = {
-  cookieTitle: "Uso de Cookies",
-  cookieMessage: "Utilizamos cookies para asegurar un mejor uso de nuestra web. Si continúas navegando, consideramos que aceptas su uso.",
-  // cookieMessageImply: "Al continuar usando nuestra web sin cambiar la configuración, estas aceptando que usemos estas cookies.",
+  cookieTitle: 'Uso de Cookies',
+  cookieMessage: 'Utilizamos cookies para asegurar un mejor uso de nuestra web. Si continúas navegando, consideramos que aceptas su uso.',
+  // cookieMessageImply: 'Al continuar usando nuestra web sin cambiar la configuración, estas aceptando que usemos estas cookies.',
   showLink: false,
   position: 'top',
-  linkText: "Lee más",
-  linkRouteName: "/",
-  acceptButtonText: "Aceptar",
+  linkText: 'Lee más',
+  linkRouteName: '/',
+  acceptButtonText: 'Aceptar',
   html: false,
   expirationInDays: 70
 };
 
-// CookieConsent.init();
-CookieConsent._config = options;
+CookieConsent.init(options);
