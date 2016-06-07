dbd4842fdb7d2d8254d3e3e3dc926ab8d686e33d
vjrj <vjrj@ourproject.org>
2016-06-07 11:50:30 +0200

    Improved dialog

--- client/views/users/casosReportados.js
+++ client/views/users/casosReportados.js
@@ -1,7 +1,10 @@
-/* global Template Persons */
+/* global Template Persons noUndef */
 Template.casosReportados.helpers({
   tieneBebes: function () {
     return Persons.find({ familiar: this.user._id }).count() > 0;
+  },
+  tieneFechaNacimiento: function (bebe) {
+    return typeof bebe.fechaNacimiento !== 'undefined';
   },
   bebes: function () {
     return Persons.find({ familiar: this.user._id });
