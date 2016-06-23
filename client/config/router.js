32b19b1bdd4145093d3346ef734163a5e899bf77
vjrj <vjrj@ourproject.org>
2016-06-23 20:44:12 +0200

    Updating of autoscroll

--- client/config/router.js
+++ client/config/router.js
@@ -1,5 +1,8 @@
+/* global Router RouterAutoscroll */
 Router.configure({
-    layoutTemplate: 'basicLayout',
-    loadingTemplate: "loading",
-    notFoundTemplate: "notFound"
+  layoutTemplate: 'basicLayout',
+  loadingTemplate: 'loading',
+  notFoundTemplate: 'notFound'
 });
+
+RouterAutoscroll.marginTop = 50;
