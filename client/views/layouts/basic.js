022ff4951ccd547b29c41c6c5dfe9284d81983dd
vjrj <vjrj@ourproject.org>
2016-06-23 20:43:12 +0200

    Email verification link

--- client/views/layouts/basic.js
+++ client/views/layouts/basic.js
@@ -1,10 +1,12 @@
+/* global Template Meteor success alertMessage $ Session Deps Router SEO */
 // https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
+function shuffle (array) {
+  var currentIndex = array.length;
+  var temporaryValue;
+  var randomIndex;
 
   // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
+  while (currentIndex !== 0) {
     // Pick a remaining element...
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
@@ -18,19 +20,32 @@ function shuffle(array) {
   return array;
 }
 
-Template.basicLayout.onRendered(function() {
-    // http://stackoverflow.com/questions/21203111/bootstrap-3-collapsed-menu-doesnt-close-on-click
-    $(document).on('click','.navbar-collapse.in',function(e) {
-	if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
-            $(this).collapse('hide');
-	}
+Template.basicLayout.events({
+  'click .resend-verification-link': function (event, template) {
+    Meteor.call('resendVerificationEmail', function (error, response) {
+      if (error) {
+        alertMessage(error.reason);
+      } else {
+        var email = Meteor.user().emails[0].address;
+        success('Correo de verificación enviado a ' + email);
+      }
     });
+  }
+});
+
+Template.basicLayout.onRendered(function () {
+  // http://stackoverflow.com/questions/21203111/bootstrap-3-collapsed-menu-doesnt-close-on-click
+  $(document).on('click', '.navbar-collapse.in', function (e) {
+    if ($(e.target).is('a') && $(e.target).attr('class') !== 'dropdown-toggle') {
+      $(this).collapse('hide');
+    }
+  });
 });
 
 Template.basicLayout.helpers({
   template: function () {
-    route = Router.current();
-    return route? route.lookupTemplate() : 'home';
+    var route = Router.current();
+    return route ? route.lookupTemplate() : 'home';
   },
   photos: function () {
     var ph = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99]);
@@ -41,12 +56,12 @@ Template.basicLayout.helpers({
 
 Template.basicLayout.events({
   'load #lorem-photo-100': function () {
-    $("#lorem-photos").show();
+    $('#lorem-photos').show();
   }
 });
 
 // https://stackoverflow.com/questions/14036248/meteor-setting-the-document-title
-Session.setDefault("DocumentTitle", "");
+Session.setDefault('DocumentTitle', '');
 Deps.autorun(function () {
   var newTitle = Session.get('DocumentTitle') + ' - ' + Meteor.App.NAME;
   document.title = newTitle;
--- server/config/accounts.js
+++ server/config/accounts.js
@@ -1,23 +1,25 @@
+/* global Accounts check Meteor */
 // https://stackoverflow.com/questions/26799150/meteor-accounts-via-external-services-dont-set-user-username
-var generate_username = function(username) {
+
+var generate_username = function (username) {
   var count;
-  username = username.toLowerCase().trim().replace(" ", "").replace(".", "");
-  count = Meteor.users.find({"username": username}).count();
+  username = username.toLowerCase().trim().replace(' ', '').replace('.', '');
+  count = Meteor.users.find({'username': username}).count();
   if (count === 0) {
     return username;
-  }
-  else {
+  } else {
     return username + (count + 1).toString();
   }
-}
+};
 
-var setEmail = function(user, email) {
-  if (email)
-    user.emails = [{address: email , verified: true }];
-}
+var setEmail = function (user, email) {
+  if (email) {
+    user.emails = [{address: email, verified: true}];
+  }
+};
 
 // https://stackoverflow.com/questions/30463958/how-to-extract-username-from-3rd-party-social-logins-via-useraccounts-package
-Accounts.onCreateUser(function(options, user){
+Accounts.onCreateUser(function (options, user) {
   if (user.services) {
     if (user.services.google) {
       var email = user.services.google.email;
@@ -27,20 +29,16 @@ Accounts.onCreateUser(function(options, user){
         options.profile.name = user.services.google.name;
       }
       setEmail(user, email);
-    }
-    else if (user.services.twitter) {
-      var email = user.services.twitter.email;
+    } else if (user.services.twitter) {
       user.username = generate_username(user.services.twitter.screenName);
       if (options && options.profile) {
         options.profile.name = user.services.twitter.screenName;
       }
-      setEmail(user, email);
-    }
-    else if (user.services.facebook) {
+      setEmail(user, user.services.twitter.email);
+    } else if (user.services.facebook) {
       // not tested
       user.username = user.services.facebook.username;
-    }
-    else if (user.services.github) {
+    } else if (user.services.github) {
       user.username = user.services.github.username;
     }
   }
@@ -48,40 +46,51 @@ Accounts.onCreateUser(function(options, user){
   // console.log(options.profile);
 
   // We still want the default hook's 'profile' behavior.
-  if (options && options.profile){
+  if (options && options.profile) {
     user.profile = options.profile;
   }
   return user;
 });
 
-Accounts.emailTemplates.siteName = "Registro de Bebes Robados";
-Accounts.emailTemplates.from = "noreply@comunes.org";
+Accounts.emailTemplates.siteName = 'Registro de Bebes Robados';
+
+Accounts.emailTemplates.from = 'noreply@comunes.org';
 
 Accounts.emailTemplates.enrollAccount.subject = function (user) {
-  return "Bienvenido/a " + user.profile.name + ",";
+  return 'Bienvenido/a ' + user.profile.name + ',';
 };
+
 Accounts.emailTemplates.enrollAccount.text = function (user, url) {
-  return "¡Gracias por participar en nuestra red!"
-       + " Para activar tu cuenta, simplemente pulsa en el link de abajo::\n\n"
-       + url;
+  return '¡Gracias por participar en nuestra red!' +
+       ' Para activar tu cuenta, simplemente pulsa en el link de abajo::\n\n' +
+       url;
 };
 
 Accounts.emailTemplates.resetPassword.subject = function (user) {
-  return "Resetea tu contraseña en " + Accounts.emailTemplates.siteName;
+  return 'Resetea tu contraseña en ' + Accounts.emailTemplates.siteName;
 };
+
 Accounts.emailTemplates.resetPassword.text = function (user, url) {
-  return "Muy buenas " + user.profile.username + ",\n\n"
-         + "Pulsa en el siguiente enlace para restablecer tu nueva contraseña:\n" +
-         url + "\n\n" +
-         "¡Por favor, no la vuelvas a olvidar!\n\n\n" +
-         "Un saludo\n";
+  return 'Muy buenas ' + user.profile.username + ',\n\n' +
+         'Pulsa en el siguiente enlace para restablecer tu nueva contraseña:\n' +
+         url + '\n\n' +
+         '¡Por favor, no la vuelvas a olvidar!\n\n\n' +
+         'Un saludo\n';
 };
 
 Accounts.emailTemplates.verifyEmail.subject = function (user) {
-  return "Verifica tu correo electrónico en " + Accounts.emailTemplates.siteName;
+  return 'Verifica tu correo electrónico en ' + Accounts.emailTemplates.siteName;
 };
+
 Accounts.emailTemplates.verifyEmail.text = function (user, url) {
-  return "Muy buenas,\n\n"
-       + "Para verificar tu cuenta, simplemente pulsa en el siguiente enlace:\n\n"
-       + url + "\n\n¡Gracias!";
+  return 'Muy buenas,\n\n' +
+    'Para verificar tu cuenta, simplemente pulsa en el siguiente enlace:\n\n' +
+    url + '\n\n¡Gracias!';
 };
+
+Meteor.methods({
+  resendVerificationEmail: function () {
+    check(Meteor.userId(), String);
+    Accounts.sendVerificationEmail(Meteor.userId());
+  }
+});
