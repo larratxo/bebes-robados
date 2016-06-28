cfe9f1bc1dc1b1a82c13dafc81bbb65902b07ee3
vjrj <vjrj@ourproject.org>
2016-06-28 21:05:37 +0200

    Fixtures refactor. Not load persons if test user is null

--- server/startup/loadFixtures.js
+++ server/startup/loadFixtures.js
@@ -1,52 +1,54 @@
-/*global Meteor,Accounts,console,YAML,Fixtures, webPages */
-function loadUser(user) {
-  var userAlreadyExists = typeof Meteor.users.findOne({ username : user.username }) === 'object';
+/*global Meteor,Accounts,console,YAML,Fixtures, webPages Persons Assets Provincias Municipios */
+
+var loadUser = function (user) {
+  var userAlreadyExists = typeof Meteor.users.findOne({ username: user.username }) === 'object';
 
   if (!userAlreadyExists) {
     Accounts.createUser(user);
-    console.log("Creating user " + user.username);
+    console.log('Creating user ' + user.username);
   }
-}
+};
 
-function loadFixture(fixtures, collection) {
-  for (var i = 0; i < fixtures.length; i+= 1) {
-    //collection.remove({ });
+var loadFixture = function (fixtures, collection) {
+  for (var i = 0; i < fixtures.length; i += 1) {
+    // collection.remove({ });
     collection.insert(fixtures[i]);
   }
-}
+};
 
 Meteor.startup(function () {
-    if (!Meteor.settings.public.isProduction) {
-  var users = YAML.eval(Assets.getText('users.yml'));
+  if (!Meteor.settings.public.isProduction) {
+    var users = YAML.eval(Assets.getText('users.yml'));
 
-  for (var key in users) {
-    if (users.hasOwnProperty(key)) {
-      loadUser(users[key]);
+    for (var key in users) {
+      if (users.hasOwnProperty(key)) {
+        loadUser(users[key]);
+      }
+    }
+
+    var testUserId = Meteor.users.findOne({username: 'test'})._id;
+
+    if (testUserId !== null && Persons.find().count() === 0) {
+      console.log('Loading persons fixtures for user ' + testUserId);
+      loadFixture(Fixtures.persons, Persons);
+      Persons.update({}, {$set: {familiar: testUserId}}, { multi: true });
+    } else {
+      // console.log('Not loading persons fixtures.');
     }
   }
-
-  var testUserId = Meteor.users.findOne( { username : "test" } )._id;
-
-  if (Persons.find().count() === 0) {
-    console.log("Loading persons fixtures for user " + testUserId);
-    loadFixture(Fixtures.persons, Persons);
-    Persons.update({}, {$set: {familiar: testUserId}}, { multi: true });
-  } else {
-    // console.log("Not loading persons fixtures.");
-  }
-    }
   if (Provincias.find().count() === 0) {
-    console.log("Loading provinces fixtures.");
+    console.log('Loading provinces fixtures.');
     var p = Fixtures.provincias;
-    for (var i = 0; i < p.length; i+= 1) {
-      Provincias.insert({"code": parseInt(p[i].code), "name": p[i].name});
+    for (var i = 0; i < p.length; i += 1) {
+      Provincias.insert({'code': parseInt(p[i].code, 10), 'name': p[i].name});
     }
   }
   if (Municipios.find().count() === 0) {
-    console.log("Loading municipes fixtures.");
+    console.log('Loading municipes fixtures.');
     var m = Fixtures.municipios;
-    for (var i = 0; i < m.length; i+= 1) {
-      Municipios.insert({cod_id: parseInt(m[i].id), cod_prov : parseInt(m[i].cod_prov), cod_mun : parseInt(m[i].cod_mun), name: m[i].name });
+    for (var j = 0; j < m.length; j += 1) {
+      Municipios.insert({cod_id: parseInt(m[j].id, 10), cod_prov: parseInt(m[j].cod_prov, 10),
+                         cod_mun: parseInt(m[j].cod_mun, 10), name: m[j].name });
     }
   }
 
@@ -59,5 +61,4 @@ Meteor.startup(function () {
       webPages.insert({title: title, text: 'Pulsa con el ratón para editar esta página'});
     }
   }
-
 });
