9e378dd4e8feb432caa368e887d9f9c466bd470e
vjrj <vjrj@ourproject.org>
2016-06-29 12:03:50 +0200

    Adcampaings now use cropper

--- client/views/difu/DifuEditor.js
+++ client/views/difu/DifuEditor.js
@@ -1,7 +1,9 @@
-/* global Template, ReactiveVar, $, Meteor, AdCampaigns, Persons, _ UniHTML alertMessage success */
+/* global Template, ReactiveVar, $, Meteor, AdCampaigns, Persons, _ UniHTML alertMessage success FileReader Blob */
 
 var getAfectado = function () {
-  return Persons.find({familiar: Meteor.userId()});
+  return Persons.find({
+    familiar: Meteor.userId()
+  });
 };
 
 Template.registerHelper('afectado', function () {
@@ -9,83 +11,142 @@ Template.registerHelper('afectado', function () {
 });
 
 Template.DifuEditor.helpers({
-  participa: function () { return Template.instance().myAd.get().participate ? 'checked' : ''; },
-  participaClass: function () { return Template.instance().myAd.get().participate ? 'difu-visible' : 'difu-hidden'; },
-  noParticipaClass: function () { return Template.instance().myAd.get().participate ? 'difu-hidden' : 'difu-visible'; },
-  dynPhoto: function () { return Template.instance().myAd.get().photo; },
+  participa: function () {
+    return Template.instance().myAd.get().participate ? 'checked' : '';
+  },
+  participaClass: function () {
+    return Template.instance().myAd.get().participate ? 'difu-visible' : 'difu-hidden';
+  },
+  noParticipaClass: function () {
+    return Template.instance().myAd.get().participate ? 'difu-hidden' : 'difu-visible';
+  },
+  dynPhoto: function () {
+    return Template.instance().myAd.get().photo;
+  },
   dynHeader: function () {
     return Template.instance().myAd.get().text;
   },
-  /* https://github.com/pfafman/meteor-photo-up */
-  photoUpOptions: function () {
-    var templ = Template.instance();
-    return {
-      loadImage: { maxWidth: 600, mixHeight: 500, cover: true, aspectRatio: 6 / 5 },
-      showInfo: false,
-      showClear: true,
-      autoSelectOnJcrop: true,
-      crop: true,
-      jCrop: { setSelect: [0, 0, 600, 500], minSize: [450, 375], maxSize: [900, 750], aspectRatio: 6 / 5 },
-      callback: function (error, photo) {
-        if (error == null) {
-          var myAd = templ.myAd.get();
-          myAd.photo = photo.src;
-          myAd.photoHD = photo.img;
-          // console.log("foto" + photo.img); Es un HTMLCanvasElement
-          AdCampaigns.update(myAd._id, {$set: {photo: photo.src, validated: false}});
-          templ.myAd.set(myAd);
-        }
-      }
-    };
+  notCropping: function () {
+    return !Template.instance().cropping.get();
   }
 });
 
 Template.DifuEditor.onCreated(function () {
-  var ad = AdCampaigns.findOne({user: Meteor.userId()});
+  var ad = AdCampaigns.findOne({
+    user: Meteor.userId()
+  });
   var casos = getAfectado();
   var count = casos.count();
   var afectado = count >= 1;
   if (typeof (ad) === 'undefined' && afectado) {
     var personSlug = casos.fetch()[0].slug;
-    AdCampaigns.insert({ user: Meteor.userId(), participate: false, validated: false, text: '', bebe: personSlug });
-    ad = AdCampaigns.findOne({user: Meteor.userId()});
+    AdCampaigns.insert({
+      user: Meteor.userId(),
+      participate: false,
+      validated: false,
+      text: '',
+      bebe: personSlug
+    });
+    ad = AdCampaigns.findOne({
+      user: Meteor.userId()
+    });
   }
   this.myAd = new ReactiveVar(ad);
+  this.cropping = new ReactiveVar(false);
 });
 
 Template.DifuEditor.onRendered(function () {
-  $('div.photo-up div i').addClass('fa fa-camera');
-  $('div.photo-up div i').text('');
 });
 
-function refreshTitle (template, newText, save) {
+var uploadCallback = function (error) {
+  if (error) {
+    alertMessage(error);
+  } else {
+    success('Guardado');
+  }
+};
+
+var updateTextTh = _.throttle(function (id, newText) {
+  AdCampaigns.update(id, {
+    $set: {
+      text: newText,
+      validated: false
+    }
+  });
+  // }, uploadCallback);
+}, 4000);
+
+var updateImg = _.throttle(function (id, imgCropped) {
+  AdCampaigns.update(id, {
+    $set: {
+      photo: imgCropped,
+      validated: false
+    }
+  }, uploadCallback);
+}, 4000);
+
+function refreshTitle (e, template, newText, save) {
   var myAd = template.myAd.get();
   myAd.text = newText;
   template.myAd.set(myAd);
   if (save) {
-   // _.throttle(function () {
-    AdCampaigns.update(myAd._id, {$set: {text: newText, validated: false}}, function (error) {
-      if (error) {
-        alertMessage(error);
-      } else {
-        success('Guardado');
-      }
-    });
-    // }, 5000);
+    updateTextTh(myAd._id, newText);
   }
 }
 
 Template.DifuEditor.events({
+  'change #files': function (e, template) {
+    // https://stackoverflow.com/questions/14069421/show-an-image-preview-before-upload
+    var reader = new FileReader();
+    reader.onload = function (e) {
+      // get loaded data and render thumbnail.
+      var imgCropSelector = $('#cropperimage');
+      imgCropSelector.attr('src', e.target.result);
+      imgCropSelector.cropper({
+        viewMode: 0,
+        autoCropArea: 0.7,
+        aspectRatio: 6 / 5,
+        /* autoCrop: false, */
+        responsive: true,
+        dragMode: 'move',
+        // minCropBoxWidth: 450,
+        // minCropBoxHeight: 375,
+        minCropBoxWidth: 300,
+        minCropBoxHeight: 250,
+        background: false,
+        crop: function (e) {
+          template.cropping.set(true);
+          var imgCropped = imgCropSelector.cropper('getCroppedCanvas').toDataURL();
+          var myAd = template.myAd.get();
+          myAd.photo = imgCropped;
+          // myAd.photoHD = photo.img;
+          // console.log("foto" + photo.img); Es un HTMLCanvasElement
+          updateImg(myAd._id, imgCropped);
+          template.myAd.set(myAd);
+        }
+      });
+    };
+    // read the image file as a data URL.
+    // console.log(e.currentTarget.files);
+    if (e.currentTarget.files[0] instanceof Blob) {
+      reader.readAsDataURL(e.currentTarget.files[0]);
+    }
+  },
   'change #participar': function (e, template) {
     var myAd = template.myAd.get();
     myAd.participate = e.target.checked;
     template.myAd.set(myAd);
-    AdCampaigns.update(myAd._id, {$set: {participate: e.target.checked, validated: false}});
+    AdCampaigns.update(myAd._id, {
+      $set: {
+        participate: e.target.checked,
+        validated: false
+      }
+    });
   },
   'focusout #adtitle, change #adtitle': _.throttle(function (e, template) {
-    refreshTitle(template, UniHTML.purify(e.target.value), true);
+    refreshTitle(e, template, UniHTML.purify(e.target.value), true);
   }, 300),
   'keyup #adtitle': _.throttle(function (e, template) {
-    refreshTitle(template, UniHTML.purify(e.target.value), false);
+    refreshTitle(e, template, UniHTML.purify(e.target.value), false);
   }, 300)
 });
