ccc79d9242d0bb0485c63e6ce014a65fba01cace
vjrj <vjrj@ourproject.org>
2016-07-06 09:54:45 +0200

    Attachs downloads

--- client/views/persons/filesDownload.js
+++ client/views/persons/filesDownload.js
@@ -1,9 +1,21 @@
-/* global Template ReactiveVar aFilesCollection */
+/* global Template ReactiveVar aFilesCollection _ */
 Template.filesDownload.onCreated(function () {
   this.collection = new ReactiveVar(aFilesCollection[this.data.collection]);
 });
 
 Template.filesDownload.helpers({
+  files: function () {
+    var array = Template.instance().data.uploadedFiles;
+    var hasFiles = _.isArray(array) && array.length > 0;
+    if (hasFiles) {
+      if (typeof array[0] === 'string') {
+        return Template.instance().collection.get().find({ _id: { $in: array } });
+      } else {
+        return array;
+      }
+    }
+    return [];
+  },
   fileLink: function (fileRef) {
     return Template.instance().collection.get().link(fileRef);
   }
